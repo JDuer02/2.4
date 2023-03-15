37226d5cc4983315a5a92b71c26ebd7ead02f98c
JDuer02 <126687677+JDuer02@users.noreply.github.com>
2023-03-15 13:46:08 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Wybierz opcję: 1. Pole prostokąta    2. Pole trójkąta");
+    int opcja = scanner.nextInt();
+    switch(opcja)
+      {
+        case 1:
+          System.out.println("Podaj wymiary prostokąta: ");
+          double a = scanner.nextDouble();
+          double b = scanner.nextDouble();
+          System.out.println(a*b);
+
+        case 2:
+          System.out.println("Podaj długości przyprostokątnych: ");
+          double x = scanner.nextDouble();
+          double y = scanner.nextDouble();
+          System.out.println(0.5*x*y);
+      }
+  }
+}
